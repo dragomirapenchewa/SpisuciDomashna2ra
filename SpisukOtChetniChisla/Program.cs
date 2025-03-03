01a12a94f5beb6c161de0a9d7e0272eb222e4a51
dragomirapenchewa <dragomiramarianovap@gmail.com>
2025-03-03 20:25:42 +0200

    Spisuci Domashna 2ra

---
+++ SpisukOtChetniChisla/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpisukOtChetniChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wuwedi chisla: ");
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int index = 0; index < nums.Count; index++)
+            {
+
+                if (nums[index] % 2 == 0)
+                {
+                    Console.WriteLine(nums[index]);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ SpisukOtKrainosti/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpisukOtKrainosti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wuwedi chisla:");
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            int min = nums[0];
+            int max = nums[0];
+
+            for (int i = 1; i < nums.Count; i++) 
+            {
+                if (nums[i] < min) 
+                {
+                    min = nums[i];
+                }
+                if (nums[i] > max) 
+                {
+                    max = nums[i]; 
+                }
+
+            }
+
+            for (int index = 0; index < nums.Count; index++)
+            {
+
+                if (nums[index] == min || nums[index] == max)
+                {
+                    result.Add(index);  
+                }
+            }
+
+            for (int index = 0; result.Count > 0; index++)
+            {
+                Console.WriteLine(result[index]);
+            }
+
+        }
+    }
+}
