f2e0a2aa74c7039c336f2fa9b7b5c29ce7f4bdc1
dragomirapenchewa <dragomiramarianovap@gmail.com>
2025-03-12 17:14:08 +0200

    Second

---
+++ Pozdraw/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pozdraw
+{
+    internal class Program
+    {
+        static void Pozdraw(string name)
+        {
+            
+            Console.WriteLine("Zdraweite, " + name );
+        }
+        static void Main(string[] args)
+        {
+            Pozdraw("Tonq Belezirewa");
+            Pozdraw("Uchenici");
+        }
+    }
+}
